61603049c6ee7e88b310f46aceccb62667dbd519
Bimo <bimosrtno@gmail.com>
2025-04-05 20:06:34 +0700

    minor change

--- BE/app.js
+++ BE/app.js
@@ -8,6 +8,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.get ("/api/hello", (req, res) => res.send("Hello World!"));
 
 // Export app untuk server.js
 module.exports = app;
